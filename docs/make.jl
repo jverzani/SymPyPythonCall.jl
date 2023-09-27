eaa1cfdd69832f081065aad0e9c44f1db8444569
jverzani <jverzani@gmail.com>
2023-09-26 20:33:08 -0400

    workaround doc errors

--- docs/make.jl
+++ docs/make.jl
@@ -9,6 +9,7 @@ makedocs(
     sitename = "SymPyPythonCall",
     format = Documenter.HTML(),
     modules = [SymPyPythonCall]
+    warnonly = [:missing_docs],
 )
 
 # Documenter can also automatically deploy documentation to gh-pages.
@@ -17,27 +18,3 @@ makedocs(
 deploydocs(
     repo = "github.com/jverzani/SymPyPythonCall.jl.git"
 )
-
-
-#DocMeta.setdocmeta!(SymPyPythonCall, :DocTestSetup, :(using SymPyPythonCall); recursive=true)
-
-# makedocs(;
-#     modules=[SymPyPythonCall],
-#     authors="jverzani <jverzani@gmail.com> and contributors",
-#     repo="https://github.com/jverzani/SymPyPythonCall.jl/blob/{commit}{path}#{line}",
-#     sitename="SymPyPythonCall.jl",
-#     format=Documenter.HTML(;
-#         prettyurls=get(ENV, "CI", "false") == "true",
-#         canonical="https://jverzani.github.io/SymPyPythonCall.jl",
-#         assets=String[],
-#     ),
-#     pages=[
-#         "Home" => "index.md",
-#         "Examples" => "introduction.md"
-#     ],
-# )
-
-# # deploydocs(;
-#     repo="github.com/jverzani/SymPyPythonCall.jl",
-#     devbranch="main",
-# )
