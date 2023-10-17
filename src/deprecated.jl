8b44b586aef9a0940f78f83dffa1ab5198f094d7
jverzani <jverzani@gmail.com>
2023-10-17 08:06:03 -0400

    _sympy_

--- src/deprecated.jl
+++ src/deprecated.jl
@@ -3,8 +3,6 @@
 # [Symbol("@syms"), :Differential, :E, :Eq, :FALSE, :Ge, :Gt, :IM, :Le, :Lt, :N, :Ne, :PI, :Sym, :SymPyPythonCall, :TRUE, :VectorField, :Wild, :apart, :ask, :cancel, :degree, :doit, :dsolve, :elements, :expand, :expand_trig, :factor, :free_symbols, :hessian, :integrate, :lambdify, :lhs, :limit, :linsolve, :nonlinsolve, :nroots, :nsolve, :oo, :plot_implicit, :plot_parametric_surface, :real_roots, :refine, :rewrite, :rhs, :roots, :series, :simplify, :solve, :solveset, :subs, :summation, :symbols, :sympy, :sympy_plotting, :together, :zoo, :¬, :∧, :∨, :≦, :≧, :≪, :≫, :≶, :≷, :⩵, :𝑄]
 
 Base.@deprecate expand_trig(x) x.expand_trig() true
-
-Base.@deprecate_binding _sympy_ sympy.o
 Base.@deprecate_binding Q 𝑄
 
 function unSym(x)
